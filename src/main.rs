f92eb36c30e4fb8e6be8c7e63a203b2419e77bb5
MammosGeorgios <mammosgeorgios@gmail.com>
2025-05-08 19:53:21 +0300

    a loop!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    static ITERATIONS: i32 = 15;
+
+    println!("Welcome to Rust Dumper");
+    println!("First method call! my_number = {}", get_a_number());
+    call_a_loop(ITERATIONS);
+
+    println!(concat!(
+        "You can concat string with the macro concat! ",
+        stringify!(What on earth is stringify though)
+    ));
+}
+
+fn get_a_number() -> i32 {
+    let my_number: i32 = 1821;
+    my_number // woah look at this return
+}
+
+fn call_a_loop(iterations: i32) {
+    for _i in 0..iterations {
+        println!("Oh wow a loop, with number {}", _i);
+    }
 }
